273e030748a18adcc0e9bda2e06f76a0152d177e
StefanutVlad <53474954+StefanutVlad@users.noreply.github.com>
2021-06-30 01:56:42 +0300

    Update config.example.js

--- emergency-system-backend/config/config.example.js
+++ emergency-system-backend/config/config.example.js
@@ -1,4 +1,4 @@
-module.exports = {
-  db: 'mongodb://username:password@url:port/db',
-  db_dev: 'mongodb://url:port/db',
+export default {
+  db: "mongodb://username:password@url:port/db",
+  db_dev: "mongodb://url:port/db",
 };
